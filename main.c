3bf819efda2ecb3daf7b95e07538f240d4ffb0c8
sasensio <sasensio>
2014-07-15 18:32:02 +0200

    Added main.c for test code

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: samuel
+ *
+ * Created on July 15, 2014, 6:31 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
